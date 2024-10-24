e89326d90796ff9aa00ecbda07228352ca72d59a
lucassbenfica <lucas.torquato@fatec.sp.gov.br>
2024-10-24 10:14:35 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// Wait for the document to be ready
+document.addEventListener('DOMContentLoaded', function () {
+  // Create a map instance and set the initial view coordinates and zoom level
+  var map = L.map('map').setView([-23.589316, -46.659423], 13);
+
+
+  // Add a tile layer to the map from OpenStreetMap
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  // Create a marker with popup and add it to the map
+  var marker = L.marker([-23.589316, -46.659423]).addTo(map);
+  marker.bindPopup("hey hey doggy").openPopup();
+});
